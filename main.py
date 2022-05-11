a140bac28be19cf9b9ed771cded103be63efa97a
ChenSk12 <1649537125@qq.com>
2022-05-11 17:19:54 +0800

    对抗训练

---
+++ main.py
@@ -0,0 +1,134 @@
+import argparse
+import os
+import torch.utils.data as Data
+
+import torchvision.utils
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import config as cf
+from networks import *
+from tqdm import tqdm
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning_rate')
+parser.add_argument('--net_type', default='wide-resnet', type=str, help='model')
+parser.add_argument('--depth', default=34, type=int, help='depth of model')
+parser.add_argument('--widen_factor', default=10, type=int, help='width of model')
+parser.add_argument('--dropout', default=0.3, type=float, help='dropout_rate')
+parser.add_argument('--dataset', default='cifar10', type=str, help='dataset = [cifar10/cifar100]')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+parser.add_argument('--testOnly', '-t', action='store_true', help='Test mode with the saved model')
+args = parser.parse_args()
+
+
+use_cuda = True
+device = torch.device("cuda" if use_cuda else "cpu")
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+]) # meanstd transformation
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+])
+start_epoch, num_epochs, batch_size, optim_type = cf.start_epoch, cf.num_epochs, cf.batch_size, cf.optim_type
+num_classes = 10
+
+
+def pgd_attack(model, images, labels, eps=0.3, alpha=2 / 255, iters=7):
+    images = images.to(device)
+    labels = labels.to(device)
+    loss = nn.CrossEntropyLoss()
+
+    ori_images = images.data
+
+    for i in range(iters):
+        images.requires_grad = True
+        outputs = model(images)
+
+        model.zero_grad()
+        cost = loss(outputs, labels).to(device)
+        cost.backward()
+
+        adv_images = images + alpha * images.grad.sign()
+        eta = torch.clamp(adv_images - ori_images, min=-eps, max=eps)
+        images = torch.clamp(ori_images + eta, min=0, max=1).detach_()
+
+    return images
+
+
+def getNetwork(args):
+    if (args.net_type == 'lenet'):
+        net = LeNet(num_classes)
+        file_name = 'lenet'
+    elif (args.net_type == 'vggnet'):
+        net = VGG(args.depth, num_classes)
+        file_name = 'vgg-'+str(args.depth)
+    elif (args.net_type == 'resnet'):
+
+        net = ResNet(args.depth, num_classes)
+        file_name = 'resnet-'+str(args.depth)
+    elif (args.net_type == 'wide-resnet'):
+        net = Wide_ResNet(args.depth, args.widen_factor, args.dropout, num_classes)
+        file_name = 'wide-resnet-34x107-PGD'
+    else:
+        print('Error : Network should be either [LeNet / VGGNet / ResNet / Wide_ResNet')
+        sys.exit(0)
+
+    return net, file_name
+
+
+_, file_name = getNetwork(args)
+checkpoint = torch.load('./checkpoint/'+args.dataset+os.sep+file_name+'.t7')
+model = checkpoint['net']
+images = torchvision.datasets.CIFAR10(root="../dataset", download=True, transform=transform_test, train=False)
+dataloader = torch.utils.data.DataLoader(images, batch_size=64, shuffle=False)
+
+model.training = False
+model.eval()
+
+correct = 0
+total = 0
+# tb = SummaryWriter('pics')
+nom = transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset])
+with torch.no_grad():
+    for i, (images, labels) in enumerate(tqdm(dataloader)):
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(nom(images))
+        # img = torchvision.utils.make_grid(images)
+        # tb.add_image("noPgd" + str(i), img)
+
+        _, pre = torch.max(outputs.data, 1)
+        total += images.size(0)
+        correct += (pre == labels).sum()
+
+print('Accuracy of test text: %f %%' % (100 * float(correct) / total))
+
+print("Attack Image & Predicted Label")
+
+correct = 0
+total = 0
+for i, (images, labels) in enumerate(tqdm(dataloader)):
+    images = pgd_attack(model, nom(images), labels)
+    labels = labels.to(device)
+    outputs = model(images)
+    # img_grid = torchvision.utils.make_grid(images)
+    # tb.add_image("Pgd" + str(i), img_grid)
+
+    _, pre = torch.max(outputs.data, 1)
+    total += images.size(0)
+    correct += (pre == labels).sum()
+# tb.close()
+print('Accuracy of test text: %f %%' % (100 * float(correct) / total))
+
+
+
+
+
+
---
+++ training.py
@@ -0,0 +1,159 @@
+import argparse
+import os
+import torch.utils.data as Data
+
+import torchvision.utils
+from torch import optim
+from torch.backends import cudnn
+from torch.utils.tensorboard import SummaryWriter
+import torchvision.transforms as transforms
+import config as cf
+import time
+from networks import *
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description='PGD-Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning_rate')
+parser.add_argument('--net_type', default='wide-resnet', type=str, help='model')
+parser.add_argument('--depth', default=34, type=int, help='depth of model')
+parser.add_argument('--widen_factor', default=10, type=int, help='width of model')
+parser.add_argument('--dropout', default=0.3, type=float, help='dropout_rate')
+parser.add_argument('--dataset', default='cifar10', type=str, help='dataset = [cifar10/cifar100]')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+args = parser.parse_args()
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+])
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(cf.mean[args.dataset], cf.std[args.dataset]),
+])
+net = Wide_ResNet(args.depth, args.widen_factor, args.dropout, 10)
+file_name = 'wide-resnet-' + str(args.depth) + 'x' + str(args.widen_factor) + '7-PGD'
+use_cuda = torch.cuda.is_available()
+best_acc = 0
+start_epoch, num_epochs, batch_size, optim_type = cf.start_epoch, cf.num_epochs, cf.batch_size, cf.optim_type
+criterion = nn.CrossEntropyLoss()
+images = torchvision.datasets.CIFAR10(root="../dataset", download=True, transform=transform_train, train=True)
+test_image = torchvision.datasets.CIFAR10(root="../dataset", download=True, transform=transform_test, train=False)
+test_loader = torch.utils.data.DataLoader(test_image, batch_size=batch_size, shuffle=False)
+dataloader = torch.utils.data.DataLoader(images, batch_size=batch_size, shuffle=True)
+device = torch.device("cuda" if use_cuda else "cpu")
+
+
+def pgd_attack(model, images, labels, eps=0.3, alpha=2 / 255, iters=7):
+    images = images.to(device)
+    labels = labels.to(device)
+    loss = nn.CrossEntropyLoss()
+
+    ori_images = images.data
+
+    for i in range(iters):
+        images.requires_grad = True
+        outputs = model(images)
+
+        model.zero_grad()
+        cost = loss(outputs, labels).to(device)
+        cost.backward()
+
+        adv_images = images + alpha * images.grad.sign()
+        eta = torch.clamp(adv_images - ori_images, min=-eps, max=eps)
+        images = torch.clamp(ori_images + eta, min=0, max=1).detach_()
+
+    return images
+
+
+# Training
+def train(epoch, tb):
+    net.to(device)
+    net.train()
+    net.training = True
+    train_loss = 0
+    correct = 0
+    total = 0
+    optimizer = optim.SGD(net.parameters(), lr=cf.learning_rate(args.lr, epoch), momentum=0.9, weight_decay=5e-4)
+
+    print('\n=> Training Epoch #%d, LR=%.4f' % (epoch, cf.learning_rate(args.lr, epoch)))
+    for batch_idx, (inputs, targets) in enumerate(dataloader):
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()  # GPU settings
+        optimizer.zero_grad()
+        inputs = pgd_attack(net, inputs, targets)
+        inputs, targets = Variable(inputs), Variable(targets)
+        outputs = net(inputs)  # Forward Propagation
+        loss = criterion(outputs, targets)  # Loss
+        loss.backward()  # Backward Propagation
+        optimizer.step()  # Optimizer update
+
+        train_loss += loss.item()
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += predicted.eq(targets.data).cpu().sum()
+
+        sys.stdout.write('\r')
+        sys.stdout.write('| Epoch [%3d/%3d] Iter[%3d/%3d]\t\tLoss: %.4f Acc@1: %.3f%%'
+                         % (epoch, num_epochs, batch_idx + 1,
+                            (len(images) // batch_size) + 1, loss.item(), 100. * correct / total))
+        sys.stdout.flush()
+    tb.add_scalar("Loss", train_loss, epoch)
+    tb.add_scalar("Accuracy", correct / len(images), epoch)
+
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    net.training = False
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            if use_cuda:
+                inputs, targets = inputs.cuda(), targets.cuda()
+            inputs, targets = Variable(inputs), Variable(targets)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += targets.size(0)
+            correct += predicted.eq(targets.data).cpu().sum()
+
+        # Save checkpoint when best model
+        acc = 100. * correct / total
+        print("\n| Validation Epoch #%d\t\t\tLoss: %.4f Acc@1: %.2f%%" % (epoch, loss.item(), acc))
+
+        if acc > best_acc:
+            print('| Saving Best model...\t\t\tTop1 = %.2f%%' % (acc))
+            state = {
+                'net':net.module if use_cuda else net,
+                'acc': acc,
+                'epoch': epoch,
+            }
+            if not os.path.isdir('checkpoint'):
+                os.mkdir('checkpoint')
+            save_point = './checkpoint/' + 'cifar10' + os.sep
+            if not os.path.isdir(save_point):
+                os.mkdir(save_point)
+            torch.save(state, save_point + file_name + '.t7')
+            best_acc = acc
+
+
+if use_cuda:
+    net.cuda()
+    net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+    cudnn.benchmark = True
+elapsed_time = 0
+tb = SummaryWriter('7pgd')
+for epoch in range(start_epoch, start_epoch + 2):
+    start_time = time.time()
+    train(epoch, tb)
+    test(epoch)
+    epoch_time = time.time() - start_time
+    elapsed_time += epoch_time
+    print('| Elapsed time : %d:%02d:%02d' % (cf.get_hms(elapsed_time)))
+tb.close()
